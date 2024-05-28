d098b7b1e3340669bcce27cdc0526249e09a3733
Marcin <marcin@mbp-marcin.home>
2024-05-28 11:36:58 +0200

    Pierwsza rewizja

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using sulmar_blazor_htmx.Components;
+using sulmar_blazor_htmx.Components.Pages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapGet("/love-htmx", 
+    () => new RazorComponentResult<LoveHtmx>());
+app.Run();
